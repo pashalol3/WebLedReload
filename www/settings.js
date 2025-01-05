cb5beec2e40798f2ccd082cd00d5365a67cc8f89
Pavel <pashalol3@mail.ru>
2025-01-06 01:04:44 +0300

    Правльное вычисление индексов матрицы.

--- www/settings.js
+++ www/settings.js
@@ -1,24 +1,40 @@
 "use strict";
+var MessageType;
+(function (MessageType) {
+    MessageType[MessageType["SetFullState"] = 1] = "SetFullState";
+    MessageType[MessageType["SetPointsSolidColor"] = 2] = "SetPointsSolidColor";
+    MessageType[MessageType["SetSolidColor"] = 3] = "SetSolidColor";
+    MessageType[MessageType["SetOnePixel"] = 4] = "SetOnePixel";
+    MessageType[MessageType["Settings"] = 5] = "Settings";
+})(MessageType || (MessageType = {}));
+var PixelType;
+(function (PixelType) {
+    PixelType[PixelType["SupportPoint"] = 0] = "SupportPoint";
+    PixelType[PixelType["Default"] = 1] = "Default";
+    PixelType[PixelType["Count"] = 2] = "Count";
+})(PixelType || (PixelType = {}));
+const getPixelColor = (pixel) => {
+    switch (pixel) {
+        case PixelType.SupportPoint: return '#00ff00';
+        case PixelType.Default: return '#ffffff';
+        default: throw new Error("UNREACHABLE");
+    }
+};
+const getPointerCoordinates = (canvas, event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = 'clientX' in event ? event.clientX - rect.left : event.touches[0].clientX - rect.left;
+    const y = 'clientY' in event ? event.clientY - rect.top : event.touches[0].clientY - rect.top;
+    return { x, y };
+};
 (() => {
-    let MessageType;
-    (function (MessageType) {
-        MessageType[MessageType["SetFullState"] = 1] = "SetFullState";
-        MessageType[MessageType["SetPointsSolidColor"] = 2] = "SetPointsSolidColor";
-        MessageType[MessageType["SetSolidColor"] = 3] = "SetSolidColor";
-        MessageType[MessageType["SetOnePixel"] = 4] = "SetOnePixel";
-        MessageType[MessageType["Settings"] = 5] = "Settings";
-    })(MessageType || (MessageType = {}));
-    const getPointerCoordinates = (canvas, event) => {
-        const rect = canvas.getBoundingClientRect();
-        const x = 'clientX' in event ? event.clientX - rect.left : event.touches[0].clientX - rect.left;
-        const y = 'clientY' in event ? event.clientY - rect.top : event.touches[0].clientY - rect.top;
-        return { x, y };
-    };
+    let GAP = 2;
+    let SQUARE_SIZE = 15;
+    let ROWS_COUNT = 16;
+    let COLUMNS_COUNT = 32;
     const saveButton = document.getElementById("bSave");
     const inputColumnCount = document.getElementById("columnCount");
     ;
     const inputRowCount = document.getElementById("rowCount");
-    const lableCoords = document.getElementById("coords");
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas === null || canvas === void 0 ? void 0 : canvas.getContext('2d');
     const canvasWidth = canvas.width;
@@ -26,18 +42,15 @@
     const backgroundColor = '#181818';
     const wsAddress = `ws://${window.location.hostname}/socket`;
     const ws = new WebSocket(wsAddress);
-    const defaultColor = 'red';
+    let isDown = false;
     const leds = [[]];
-    let GAP = 2;
-    let SQUARE_SIZE = 15;
-    let ROWS_COUNT = 50;
-    let COLUMNS_COUNT = 12;
-    let LED_TYPE = 'WS2812B';
+    let supportPoints = [];
+    // const finalNumbersArray = Array<number>(ROWS_COUNT * COLUMNS_COUNT).fill(0);
     //REMOVE THIS
     inputColumnCount.value = COLUMNS_COUNT.toString();
     inputRowCount.value = ROWS_COUNT.toString();
     //END REMOVE THIS
-    fillLeds(leds, defaultColor);
+    fillLeds(leds, PixelType.Default);
     previewLeds();
     ws.binaryType = 'arraybuffer';
     ws.onerror = (event) => {
@@ -51,19 +64,70 @@
         inputRowCount.value = ROWS_COUNT.toString();
     };
     saveButton === null || saveButton === void 0 ? void 0 : saveButton.addEventListener('click', handleSave);
-    canvas.addEventListener('pointermove', handlePointerMove);
-    canvas.addEventListener('touchmove', handlePointerMove);
-    function handlePointerMove(event) {
-        event.preventDefault();
+    canvas.addEventListener('pointerdown', handlePointerDown);
+    canvas.addEventListener('pointerup', handlePointerUp);
+    function handlePointerDown(event) {
+        isDown = true;
         const { x, y } = getPointerCoordinates(canvas, event);
         const col = Math.floor((x - (canvasWidth - COLUMNS_COUNT * (SQUARE_SIZE + GAP)) / 2) / (SQUARE_SIZE + GAP));
         const row = Math.floor((y - (canvasHeight - ROWS_COUNT * (SQUARE_SIZE + GAP)) / 2) / (SQUARE_SIZE + GAP));
-        if (col >= 0 && col < COLUMNS_COUNT && row >= 0 && row < ROWS_COUNT) {
-            if (lableCoords === null)
-                throw new Error('lableCoords ctx is null');
-            lableCoords.textContent = `Координаты: X:${row.toString().padStart(2, '0')} : Y:${col.toString().padStart(2, '0')}`;
+        let currentPixelType = leds[col][row];
+        let next = ++currentPixelType;
+        if (next == PixelType.Count)
+            next = 0;
+        const cords = { col, row };
+        const predicate = (s) => s.col === cords.col && s.row === cords.row;
+        const exists = supportPoints.some(s => predicate(s));
+        if (exists) {
+            supportPoints = supportPoints.filter(s => !predicate(s));
         }
+        else {
+            supportPoints.push(cords);
+        }
+        leds[col][row] = next;
+        console.table(supportPoints);
     }
+    function handlePointerUp(event) {
+        isDown = false;
+    }
+    const putNums = () => {
+        let idx = 0;
+        // Предполагается, что supportPoints - это массив упорядоченных точек
+        for (let i = 0; i < supportPoints.length - 1; i += 2) {
+            const first = supportPoints[i];
+            const second = supportPoints[i + 1];
+            if (first.col !== second.col) {
+                // Если колонки разные, рисуем по колонкам
+                idx = drawNumbersBetweenPoints(first, second, true, idx);
+            }
+            else if (first.row !== second.row) {
+                // Если строки разные, рисуем по строкам
+                idx = drawNumbersBetweenPoints(first, second, false, idx);
+            }
+        }
+    };
+    const drawNumbersBetweenPoints = (start, end, isHorizontal, idx) => {
+        const startPos = isHorizontal ? start.col : start.row;
+        const endPos = isHorizontal ? end.col : end.row;
+        const step = startPos < endPos ? 1 : -1;
+        for (let i = startPos; i !== endPos + step; i += step) {
+            if (ctx == null)
+                throw new Error("Ctx is null");
+            ctx.save();
+            const x = isHorizontal ?
+                (i * (SQUARE_SIZE + GAP) + (canvasWidth - COLUMNS_COUNT * (SQUARE_SIZE + GAP)) / 2 + SQUARE_SIZE / 12) :
+                (start.col * (SQUARE_SIZE + GAP) + (canvasWidth - COLUMNS_COUNT * (SQUARE_SIZE + GAP)) / 2 + SQUARE_SIZE / 10);
+            const y = isHorizontal ?
+                (start.row * (SQUARE_SIZE + GAP) + (canvasHeight - ROWS_COUNT * (SQUARE_SIZE + GAP)) / 2 + SQUARE_SIZE / 2) :
+                (i * (SQUARE_SIZE + GAP) + (canvasHeight - ROWS_COUNT * (SQUARE_SIZE + GAP)) / 2 + SQUARE_SIZE / 2);
+            ctx.fillStyle = '#ff0000';
+            ctx.font = '14px Arial';
+            ctx.fillText(`${idx}`, x, y);
+            ctx.restore();
+            idx++;
+        }
+        return idx; // Возвращаем обновленный индекс
+    };
     function previewLeds() {
         if (ctx === null)
             return;
@@ -85,7 +149,7 @@
             for (let row = 0; row < ROWS_COUNT; row++) {
                 const x = col * (SQUARE_SIZE + GAP) + (canvasWidth - COLUMNS_COUNT * (SQUARE_SIZE + GAP)) / 2;
                 const y = row * (SQUARE_SIZE + GAP) + (canvasHeight - ROWS_COUNT * (SQUARE_SIZE + GAP)) / 2;
-                ctx.fillStyle = defaultColor;
+                ctx.fillStyle = getPixelColor(leds[col][row]);
                 ctx.fillRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
             }
         }
@@ -113,12 +177,18 @@
         ws.send(buffer);
         previewLeds();
     }
-    function fillLeds(leds, colorHex) {
+    function fillLeds(leds, value) {
         for (let col = 0; col < COLUMNS_COUNT; col++) {
             leds[col] = [];
             for (let row = 0; row < ROWS_COUNT; row++) {
-                leds[col][row] = colorHex;
+                leds[col][row] = value;
             }
         }
     }
+    const startAnimation = () => {
+        previewLeds();
+        putNums();
+        requestAnimationFrame(startAnimation);
+    };
+    startAnimation();
 })();
--- www/settings.ts
+++ www/settings.ts
@@ -1,22 +1,41 @@
+enum MessageType {
+    SetFullState = 1,
+    SetPointsSolidColor = 2,
+    SetSolidColor = 3,
+    SetOnePixel = 4,
+    Settings = 5
+}
+
+enum PixelType {
+    SupportPoint = 0,
+    Default,
+    Count,
+}
+const getPixelColor = (pixel: PixelType): string => {
+    switch (pixel) {
+        case PixelType.SupportPoint: return '#00ff00'
+        case PixelType.Default: return '#ffffff'
+        default: throw new Error("UNREACHABLE");
+    }
+}
+const getPointerCoordinates = (canvas: HTMLCanvasElement, event: PointerEvent | TouchEvent) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = 'clientX' in event ? event.clientX - rect.left : event.touches[0].clientX - rect.left;
+    const y = 'clientY' in event ? event.clientY - rect.top : event.touches[0].clientY - rect.top;
+    return { x, y };
+}
+
 (() => {
 
-    enum MessageType {
-        SetFullState = 1,
-        SetPointsSolidColor = 2,
-        SetSolidColor = 3,
-        SetOnePixel = 4, 
-        Settings = 5
-    }
-    const getPointerCoordinates = (canvas: HTMLCanvasElement ,event: PointerEvent | TouchEvent) => {
-        const rect = canvas.getBoundingClientRect();
-        const x = 'clientX' in event ? event.clientX - rect.left : event.touches[0].clientX - rect.left;
-        const y = 'clientY' in event ? event.clientY - rect.top : event.touches[0].clientY - rect.top;
-        return {x, y};
-    }
+    let GAP = 2;
+    let SQUARE_SIZE = 15;
+
+    let ROWS_COUNT = 16;
+    let COLUMNS_COUNT = 32;
+
     const saveButton = document.getElementById("bSave");
     const inputColumnCount = document.getElementById("columnCount") as HTMLInputElement;;
     const inputRowCount = document.getElementById("rowCount") as HTMLInputElement;
-    const lableCoords = document.getElementById("coords") as HTMLLabelElement;
 
     const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
     const ctx = canvas?.getContext('2d');
@@ -26,23 +45,22 @@
 
     const wsAddress = `ws://${window.location.hostname}/socket`;
     const ws: WebSocket = new WebSocket(wsAddress);
-    const defaultColor = 'red';
-    const leds: string[][] = [[]];
+    let isDown = false;
+    const leds: PixelType[][] = [[]];
+    let supportPoints: Point[] = [];
+    // const finalNumbersArray = Array<number>(ROWS_COUNT * COLUMNS_COUNT).fill(0);
 
-    let GAP = 2;
-    let SQUARE_SIZE = 15;
 
-    let ROWS_COUNT = 50;
-    let COLUMNS_COUNT = 12;
-    let LED_TYPE = 'WS2812B';
 
     //REMOVE THIS
     inputColumnCount.value = COLUMNS_COUNT.toString();
     inputRowCount.value = ROWS_COUNT.toString();
     //END REMOVE THIS
 
-    fillLeds(leds, defaultColor);
+    fillLeds(leds, PixelType.Default);
+
     previewLeds();
+
     ws.binaryType = 'arraybuffer';
     ws.onerror = (event) => {
         throw new Error(`WebSocket Error: ${event}`);
@@ -57,26 +75,92 @@
     }
 
     saveButton?.addEventListener('click', handleSave);
-    canvas.addEventListener('pointermove', handlePointerMove);
-    canvas.addEventListener('touchmove', handlePointerMove);
+    canvas.addEventListener('pointerdown', handlePointerDown);
+    canvas.addEventListener('pointerup', handlePointerUp);
 
-
-    function handlePointerMove(event: PointerEvent | TouchEvent) {
-        event.preventDefault();
+    function handlePointerDown(event: PointerEvent | TouchEvent) {
+        isDown = true;
         const { x, y } = getPointerCoordinates(canvas, event);
-
         const col = Math.floor((x - (canvasWidth - COLUMNS_COUNT * (SQUARE_SIZE + GAP)) / 2) / (SQUARE_SIZE + GAP));
         const row = Math.floor((y - (canvasHeight - ROWS_COUNT * (SQUARE_SIZE + GAP)) / 2) / (SQUARE_SIZE + GAP));
+        let currentPixelType = leds[col][row];
 
-        if (col >= 0 && col < COLUMNS_COUNT && row >= 0 && row < ROWS_COUNT) {
+        let next = ++currentPixelType;
+        if (next == PixelType.Count)
+            next = 0;
+        const cords = { col, row };
 
-            if (lableCoords === null)
-                throw new Error('lableCoords ctx is null');
-            lableCoords.textContent = `Координаты: X:${row.toString().padStart(2, '0')} : Y:${col.toString().padStart(2, '0')}`;
+        const predicate = (s: Point) => s.col === cords.col && s.row === cords.row;
+
+        const exists = supportPoints.some(s => predicate(s));
+        if (exists) {
+            supportPoints = supportPoints.filter(s => !predicate(s));
+        } else {
+            supportPoints.push(cords);
         }
 
+        leds[col][row] = next;
+        console.table(supportPoints);
+    }
+
+    function handlePointerUp(event: PointerEvent | TouchEvent) {
+        isDown = false;
+    }
+
+    type Point = {
+        col: number;
+        row: number;
+    };
+
+    const putNums = () => {
+        let idx = 0;
+        // Предполагается, что supportPoints - это массив упорядоченных точек
+        for (let i = 0; i < supportPoints.length - 1; i += 2) {
+            const first = supportPoints[i];
+            const second = supportPoints[i + 1];
+
+            if (first.col !== second.col) {
+                // Если колонки разные, рисуем по колонкам
+                idx = drawNumbersBetweenPoints(first, second, true, idx);
+            } else if (first.row !== second.row) {
+                // Если строки разные, рисуем по строкам
+                idx = drawNumbersBetweenPoints(first, second, false, idx);
+            }
+        }
 
     }
+
+    const drawNumbersBetweenPoints = (start: Point, end: Point, isHorizontal: boolean, idx: number): number => {
+        const startPos = isHorizontal ? start.col : start.row;
+        const endPos = isHorizontal ? end.col : end.row;
+
+        const step = startPos < endPos ? 1 : -1;
+
+        for (let i = startPos; i !== endPos + step; i += step) {
+            if (ctx == null) throw new Error("Ctx is null");
+            ctx.save();
+
+            const x = isHorizontal ?
+                (i * (SQUARE_SIZE + GAP) + (canvasWidth - COLUMNS_COUNT * (SQUARE_SIZE + GAP)) / 2 + SQUARE_SIZE / 12) :
+                (start.col * (SQUARE_SIZE + GAP) + (canvasWidth - COLUMNS_COUNT * (SQUARE_SIZE + GAP)) / 2 + SQUARE_SIZE / 10);
+
+            const y = isHorizontal ?
+                (start.row * (SQUARE_SIZE + GAP) + (canvasHeight - ROWS_COUNT * (SQUARE_SIZE + GAP)) / 2 + SQUARE_SIZE / 2) :
+                (i * (SQUARE_SIZE + GAP) + (canvasHeight - ROWS_COUNT * (SQUARE_SIZE + GAP)) / 2 + SQUARE_SIZE / 2);
+
+            ctx.fillStyle = '#ff0000';
+            ctx.font = '14px Arial';
+            ctx.fillText(`${idx}`, x, y);
+            ctx.restore();
+
+            idx++;
+        }
+
+        return idx; // Возвращаем обновленный индекс
+    }
+
+
+
 
     function previewLeds() {
         if (ctx === null)
@@ -103,7 +187,7 @@
             for (let row = 0; row < ROWS_COUNT; row++) {
                 const x = col * (SQUARE_SIZE + GAP) + (canvasWidth - COLUMNS_COUNT * (SQUARE_SIZE + GAP)) / 2;
                 const y = row * (SQUARE_SIZE + GAP) + (canvasHeight - ROWS_COUNT * (SQUARE_SIZE + GAP)) / 2;
-                ctx.fillStyle = defaultColor;
+                ctx.fillStyle = getPixelColor(leds[col][row]);
                 ctx.fillRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
             }
         }
@@ -133,14 +217,21 @@
         previewLeds();
 
     }
-    function fillLeds(leds: string[][], colorHex: string): void {
+    function fillLeds(leds: PixelType[][], value: PixelType) {
         for (let col = 0; col < COLUMNS_COUNT; col++) {
             leds[col] = [];
             for (let row = 0; row < ROWS_COUNT; row++) {
-                leds[col][row] = colorHex;
+                leds[col][row] = value;
             }
         }
     }
+
+    const startAnimation = () => {
+        previewLeds();
+        putNums();
+        requestAnimationFrame(startAnimation);
+    }
+    startAnimation();
 
 })();
 
